f446359c8d41828005a4f82341a531de8022a5ec
Damir Mustafin <frenchthebread@gmail.com>
2016-11-01 23:08:27 +0200

    io:format

--- src/emq_plugin_apinf.erl
+++ src/emq_plugin_apinf.erl
@@ -44,6 +44,7 @@ write_to_es(Log) ->
   esio:start(),
   {ok, Sock} = esio:socket("http://127.0.0.1:9200"),
   esio:put(Sock, "urn:es:mqt:analytics:1", #{type => Log, tags => [green]}),
+  io:format("writing to mongod.."),
   esio:close(Sock).
 % --- Custom functions
 
